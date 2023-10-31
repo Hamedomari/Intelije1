421b0cf8079e2b6778755d97df72f1e176d92cb5
Hamed omari <118768898+Hamedomari@users.noreply.github.com>
2023-10-31 01:33:43 +0100

    version 1

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -3,5 +3,7 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("Version 1");
         System.out.println(" version 2");
+        System.out.println("I am not nice");
+        System.out.println("I am good");
     }
 }
